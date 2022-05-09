b4e359ff96e24996af7ff9d9812ffd7ab4edf290
romachkuna <95134696+romachkuna@users.noreply.github.com>
2022-05-09 23:22:04 +0400

    Update min spanning tree.ml

--- min spanning tree.ml
+++ min spanning tree.ml
@@ -1,10 +1,10 @@
 type graph = (int * float * int) list
 
-let remove xs = let uniq_cons x xs = if List.mem x xs then xs else x :: xs in List.fold_right uniq_cons xs []
-
+(* THIS FUNCTION GIVES ME ALL SUCCESSOR NODES OF N *)
 let successors n e = List.map (fun (x,_,v) -> if x=n then v else if v = n then x else 0)
                         (List.filter (fun (q,_,w) -> q=n || w=n) e)
 
+(*THIS FUNCTION CHECKS IF THERE ARE DUPLICATES IN THE GRAPH, I USE THIS  IN BFS*)
 let rec dupExist lst =
 let rec exist elem lst =
   match lst with
@@ -15,6 +15,8 @@ let rec exist elem lst =
   | [] -> false
   | hd::tl -> (exist hd tl) || dupExist tl
 
+(*BREADTH FIRST SEARCH ALGORITHM WHICH TELLS ME IF GRAPH IS A CYCLE
+ IF IT IS - TRUE IF NOT FALSE*)
   let bfs graph = match graph with
   | [] -> false
   | graph -> let (m,b,k) = List.hd graph in
@@ -28,18 +30,8 @@ let rec exist elem lst =
       if dupExist (List.flatten (removed::[root_s])) then true else if (List.length visit) = (List.length graph) then false else bfs_helper graph (removed::[root_s]) visit
       in bfs_helper graph [[m]] []
 
-  let my_function = function
-  | [] -> raise Not_found
-  | ((x,f,y) as k )::t -> List.fold_left
-              (fun ((a,b,c),list) (q,w,r) ->
-              if  b<= w
-              then ((a,b,c),(q,w,r)::list)
-              else ((q,w,r),(a,b,c)::list)
 
-              )
-              ((x,f,y), [])
-              t
-
+(*THIS FUNCTION GIVES ME SMALLEST NODES IN ASCENDING ORDER FOR KRUSKAL'S ALGORITHM *)
   let rec extract_sort =
     let my_function = function
      | [] -> raise Not_found
@@ -59,7 +51,9 @@ let rec exist elem lst =
     in
       least2 :: extract_sort list;;
 
-let map = [(1,1.7,5);(1,1.2,6);(1,1.3,2);(5,1.4,4);(4,1.5,7);(2,1.1,3);(3,1.7,9);(9,1.8,10);(4,1.9,6)]
+
+(*MINIMAL SPANNING TREE -  USING KRUSKAL'S ALGORITHM *)
+(*THIS FUNCTION RETURNS NODES IN ANY ORDER IT COULD BE (4,5) OR (5,4)*)
 
  let mst graph =
             let smallest = extract_sort graph in
@@ -67,12 +61,3 @@ let map = [(1,1.7,5);(1,1.2,6);(1,1.3,2);(5,1.4,4);(4,1.5,7);(2,1.1,3);(3,1.7,9)
             | [] -> acc
             | h::t -> if bfs (h::acc) then mst_helper t acc else mst_helper t (h::acc)
             in mst_helper smallest []
-
-
-
-
-
-
-
-
-
